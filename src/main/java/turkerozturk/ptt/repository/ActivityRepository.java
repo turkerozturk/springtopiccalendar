62e930d7d9c9abbe8ee2d544bbb50e37ca572aab
turkerozturk <turker_ozt@yahoo.com>
2025-03-27 17:58:24 +0300

    delete unused classes

--- src/main/java/turkerozturk/ptt/repository/ActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package turkerozturk.ptt.repository;
-
-import turkerozturk.ptt.entity.Entry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityRepository extends JpaRepository<Entry, Long> {
-}
