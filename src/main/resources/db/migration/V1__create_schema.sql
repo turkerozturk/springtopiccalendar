a8f1b4b0984c74b56f7c9cb0cac5798561a57240
turkerozturk <turker_ozt@yahoo.com>
2025-03-28 15:36:04 +0300

    add first flyway database migration: database creation script(all tables).   - I decided to use flyway, to add unique constraint into entries table. we can use unique constraint definition only with table creation script. We cannot apply it to a created table afterwards.

---
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categories (id integer, name varchar(255) not null unique, primary key (id));
+CREATE TABLE topics (category_id bigint not null, id integer, description varchar(255), name varchar(255), primary key (id));
+CREATE TABLE entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    topic_id BIGINT NOT NULL,
+    date_millis_ymd BIGINT,
+    status INTEGER,
+    FOREIGN KEY (topic_id) REFERENCES topics(id),
+    UNIQUE (topic_id, date_millis_ymd)
+);
+CREATE TABLE notes (entry_id bigint not null unique, id integer, content TEXT, primary key (id));
