607514b08b1a88cbee03f06df1a6b92789c906ee
turkerozturk <turker_ozt@yahoo.com>
2025-04-06 22:21:31 +0300

    Revert "add license text to all files."
    This reverts commit 0417e50f4a84fcc8a1b30e14ad343f14b2f3d5ae.

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -1,6 +1,3 @@
--- This file is part of the DailyTopicTracker project.
--- Please refer to the project's README.md file for additional details.
--- https://github.com/turkerozturk/springtopiccalendar
 CREATE TABLE categories (id integer, name varchar(255) not null unique, primary key (id));
 CREATE TABLE topics (category_id bigint not null, id integer, description varchar(255), name varchar(255), primary key (id));
 CREATE TABLE entries (
