eaa4c99cfdc0098bd1002fb2306b7b2ddefff488
turkerozturk <turker_ozt@yahoo.com>
2025-07-30 09:39:04 +0300

    configuration: create StaticResourceConfig.java, add the folder outside of the resources folder named "topicimages" as a resource folder.
     - WebMvcConfigurer
     - String externalPath = Paths.get("topicimages").toAbsolutePath().toUri().toString();
               registry.addResourceHandler("/topicimages/**")
                       .addResourceLocations(externalPath);

---
+++ dailytopictracker/src/main/java/com/turkerozturk/dtt/configuration/externalresources/StaticResourceConfig.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the DailyTopicTracker project.
+ * Please refer to the project's README.md file for additional details.
+ * https://github.com/turkerozturk/springtopiccalendar
+ *
+ * Copyright (c) 2025 Turker Ozturk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+package com.turkerozturk.dtt.configuration.externalresources;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // "topicimages/" URL'sini "topicimages/" klasorune bagla
+        String externalPath = Paths.get("topicimages").toAbsolutePath().toUri().toString();
+        registry.addResourceHandler("/topicimages/**")
+                .addResourceLocations(externalPath);
+    }
+}
+
