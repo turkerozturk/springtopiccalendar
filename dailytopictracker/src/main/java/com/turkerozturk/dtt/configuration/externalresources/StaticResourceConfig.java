5574b826424b5491cf92a9b357d9024a72235d94
turkerozturk <turker_ozt@yahoo.com>
2025-09-27 09:17:55 +0300

    feature: caching static resources
    # This way, no need to reload the static webjars etc. from the server every time.
    # see StaticResourceConfig.java, the measurement unit is in seconds.
    static.caching.duration=86400
    - topicimages are also cached.
     - this and others:
    registry.addResourceHandler("/webjars/**")
                    .addResourceLocations("/webjars/")
                    .setCachePeriod(staticCachingDuration)
                    .resourceChain(true);

--- dailytopictracker/src/main/java/com/turkerozturk/dtt/configuration/externalresources/StaticResourceConfig.java
+++ dailytopictracker/src/main/java/com/turkerozturk/dtt/configuration/externalresources/StaticResourceConfig.java
@@ -20,21 +20,50 @@
  */
 package com.turkerozturk.dtt.configuration.externalresources;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.nio.file.Paths;
 
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
 
+    @Value("${static.caching.duration:86400}")
+    private int staticCachingDuration; // 60 * 60 * 24 * 1 = 86400 = one day
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         // "topicimages/" URL'sini "topicimages/" klasorune bagla
         String externalPath = Paths.get("topicimages").toAbsolutePath().toUri().toString();
         registry.addResourceHandler("/topicimages/**")
-                .addResourceLocations(externalPath);
+                .addResourceLocations(externalPath)
+                .setCachePeriod(staticCachingDuration);
+
+        // https://github.com/spring-projects/spring-framework/issues/29322
+        // https://www.webjars.org/documentation#springmvc
+        // https://jamesward.com/2012/04/30/webjars-in-spring-mvc/
+        // registry.addResourceHandler("/webjars/**")
+        // .addResourceLocations("classpath:/META-INF/resources/webjars/") // bu ise yaramiyor. Aasagidaki problemsiz:
+        //  Note: In a Servlet 3 container the registry.addResourceHandler line can be simplified to:
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+                .setCachePeriod(staticCachingDuration)
+                .resourceChain(true);
+              //  .addResolver(new WebJarsResourceResolver()); // soldakine gerek yok, deprecated, kirmizi cizer zaten.
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(staticCachingDuration);
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(staticCachingDuration);
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(staticCachingDuration);
+
     }
 }
 
