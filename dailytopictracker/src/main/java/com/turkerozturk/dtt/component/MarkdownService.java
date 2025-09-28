b4ef1fa90aae0a02cfe75780a32afe4059b50f27
turkerozturk <turker_ozt@yahoo.com>
2025-09-28 12:54:48 +0300

    backend, frontend, feature, fix: Adding external URLs and image URLs to entry notes and topic descriptions has been simplified.
    - The behavior of the image and link buttons in the EasyMDE toolbar has been changed.
    
    - The target blank attribute has been added to the links in the HTML code generated in MarkdownService.java using JSoup.

--- dailytopictracker/src/main/java/com/turkerozturk/dtt/component/MarkdownService.java
+++ dailytopictracker/src/main/java/com/turkerozturk/dtt/component/MarkdownService.java
@@ -3,6 +3,7 @@ package com.turkerozturk.dtt.component;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import org.jsoup.Jsoup;
@@ -25,10 +26,19 @@ public class MarkdownService {
         Node document = parser.parse(markdown);
         String unsafeHtml = renderer.render(document);
 
+        // linklere target="_blank" ekle
+        Document jsoupDoc = Jsoup.parse(unsafeHtml);
+        // zaten markdownda target blank eklemek olsa da pek konforlu degil, yorumlarda a href olarak eklenmesi tavsiye ediliyor.
+        // biz de hic markdownla ugrasmadan, ciktiyi verirken target blank ekliyoruz ve cok guzel bir cozum oldu.
+        jsoupDoc.select("a[href]").forEach(a -> a.attr("target", "_blank"));
+
         // HTML'i temizle (XSS korumasi)
-        String safeHtml = Jsoup.clean(unsafeHtml, Safelist.basicWithImages());
+        String safeHtml = Jsoup.clean(jsoupDoc.body().html(),
+                Safelist.basicWithImages().addAttributes("a", "target", "rel")); // güvenlik için rel de eklenebilir
+
 
         return safeHtml;
+
     }
 }
 
