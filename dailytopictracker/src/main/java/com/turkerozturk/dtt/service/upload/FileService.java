f575448fbd626f095f58985e013a0a1eb1bb0910
turkerozturk <turker_ozt@yahoo.com>
2025-09-30 17:54:32 +0300

    backend, frontend, feature: File Manager with workaround.
    The button visible in the root and first-depth folder structures of the file manager has been placed as a workaround.
    
    The button visible at subsequent depths is the normal button.
    
    Both have the same appearance; only one is visible at a time, while the other is invisible.
    
    Common navigation of the app has also been added.

--- dailytopictracker/src/main/java/com/turkerozturk/dtt/service/upload/FileService.java
+++ dailytopictracker/src/main/java/com/turkerozturk/dtt/service/upload/FileService.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the DailyTopicTracker project.
+ * Please refer to the project's README.md file for additional details.
+ * https://github.com/turkerozturk/springtopiccalendar
+ *
+ * Copyright (c) 2025 Turker Ozturk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
 package com.turkerozturk.dtt.service.upload;
 
 // FileService.java
@@ -34,17 +54,17 @@ public class FileService {
     }
 
     private Path resolveSafe(String relative) {
-        System.out.println("FileService.java içindeki resolveSafe metoduna gelen değer: "+ relative);
+        //System.out.println("FileService.java icindeki resolveSafe metoduna gelen deger: "+ relative);
 
         if (relative == null || relative.isBlank()) {
-            System.out.println("dönen değer baseDir: " + baseDir);
+            //System.out.println("donen deger baseDir: " + baseDir);
             return baseDir;
         }
         Path target = baseDir.resolve(relative).normalize();
         if (!target.startsWith(baseDir)) {
             throw new SecurityException("Invalid path");
         }
-        System.out.println("dönen değer target: " + target);
+        //System.out.println("donen deger target: " + target);
         return target;
     }
 
